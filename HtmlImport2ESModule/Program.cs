6507448d2ee46966c68a9f8c8fa80504e36ed256
Keith Henry <keithhenry@gmail.com>
2018-10-30 19:44:16 +0000

    Add project files.

---
+++ HtmlImport2ESModule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HtmlImport2ESModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
