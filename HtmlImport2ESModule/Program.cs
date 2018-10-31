6037b7e667aa4e95ae6d80dc5b84cadf4e9aca7d
Keith Henry <keithhenry@gmail.com>
2018-10-31 16:04:40 +0000

    Added ability to read HTML files direct.

--- HtmlImport2ESModule/Program.cs
+++ HtmlImport2ESModule/Program.cs
@@ -39,6 +39,11 @@ namespace HtmlImport2ESModule
             @"<style\s*(?>include=""(?<include>[\w \-]*)"")?\s*>\s*(?<css>.*)\s*<\/style>",
             RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
+        /// <summary>Look for: &lt;script&gt;target&lt;/script&gt;</summary>
+        static readonly Regex ScriptPattern = new Regex(
+            @"<script[\w\s\""\=]*>\s*(?<script>.*)\s*<\/script>",
+            RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
         static void WriteColour(string message, ConsoleColor background, ConsoleColor foreground)
         {
             var bg = Console.BackgroundColor;
@@ -83,155 +88,214 @@ namespace HtmlImport2ESModule
                 RecurseSubdirectories = true
             });
 
+            int jsCount = 0, htmlCount = 0;
             foreach (var f in files)
             {
+                jsCount++;
                 string jsRelativeFile = Path.GetFileName(f);
                 string templateFilename = f.Substring(0, f.Length - 2) + "html";
                 if (!File.Exists(templateFilename))
-                    continue;
+                    continue; // No HTML template to import
 
                 Console.WriteLine($"Parsing: {f}");
 
-                string js = File.ReadAllText(f);
-                var matchDomIs = DomModuleIsPattern.Match(js);
-                if (!matchDomIs.Success)
+                string jsFile = File.ReadAllText(f);
+
+                (string failReason, string js) = ParseHtmlImport(templateFilename, library, jsFile);
+                if (!string.IsNullOrEmpty(failReason))
                 {
-                    WriteWarn($"\tNo DOM static is property found on class.");
+                    WriteWarn("\t" + failReason);
+                    continue;
+                }
+                
+
+                //WriteColour(js, ConsoleColor.Gray, ConsoleColor.DarkGray);
+
+                File.WriteAllText(f, js, Encoding.UTF8);
+                File.Delete(templateFilename);
+                WriteSuccess($"\tTemplate ({js.Length} chars) Moved from:\r\n\t\t{templateFilename} to\r\n\t\t{f}");
+            }
+
+
+            var htmlFiles = Directory.GetFileSystemEntries(target, "*.html", new EnumerationOptions
+            {
+                MatchCasing = MatchCasing.CaseInsensitive,
+                RecurseSubdirectories = true
+            });
+
+            foreach (var f in htmlFiles)
+            {
+                htmlCount++;
+                Console.WriteLine($"Parsing: {f}");
+                (string failReason, string js) = ParseHtmlImport(f, library);
+                if (!string.IsNullOrEmpty(failReason))
+                {
+                    WriteWarn("\t" + failReason);
                     continue;
                 }
 
-                var matchDomIsGroup = matchDomIs.Groups["is"];
-                if (!matchDomIsGroup.Success)
-                {
-                    WriteWarn($"\tID string not found in {matchDomIs.Value}.");
+                //WriteColour(js, ConsoleColor.Gray, ConsoleColor.DarkGray);
+
+                string jsOutputFilename = f.Substring(0, f.Length - 4) + "js";
+                File.WriteAllText(jsOutputFilename, js, Encoding.UTF8);
+                File.Delete(f);
+                WriteSuccess($"\tTemplate ({js.Length} chars) Moved from:\r\n\t\t{f} to\r\n\t\t{jsOutputFilename}");
+            }
+
+            Console.WriteLine($"JS: {jsCount}, HTML: {htmlCount}");
+            Console.ReadLine();
+        }
+
+        /// <summary>Parse JS for the DOM module property, and return content before and after the declaration.</summary>
+        /// <param name="js">The JS to parse.</param>
+        /// <returns>Tuple, holding fail reason (if any), module ID, before content and after content.</returns>
+        static (string failReason, string domModule, string before, string after) ParseJSContent(string js) {
+            var matchDomIs = DomModuleIsPattern.Match(js);
+            if (!matchDomIs.Success)
+                return ($"\tNo DOM static is property found on class.", null, null, null);
+
+            var matchDomIsGroup = matchDomIs.Groups["is"];
+            if (!matchDomIsGroup.Success)
+                return ($"\tID string not found in {matchDomIs.Value}.", null, null, null);
+
+            string domModule = matchDomIsGroup.Value;
+
+            // Get string before and after where we'll insert the template
+            string before = js.Substring(0, matchDomIs.Index + matchDomIs.Length);
+            string after = js.Substring(matchDomIs.Index + matchDomIs.Length);
+
+            // In the before remove and //@ts-check, as we'll add at start
+            before = TSCheckPattern.Replace(before, "");
+
+            // Replace the classname.
+            before = before.Replace("Polymer.Element", "PolymerElement");
+
+            return (null, domModule, before, after);
+        }
+
+        /// <summary>Parse a Polymer 2 HTML import file and output a Polymer 3</summary>
+        /// <param name="templateFilename">The path to the HTML file to read.</param>
+        /// <param name="library">Name of the folder to check for the library components.</param>
+        /// <param name="js">Any JS read from the accompanying file.</param>
+        /// <returns>The fail reason and the output JS with the embedded template.</returns>
+        static (string failReason, string js) ParseHtmlImport(string templateFilename, string library, string js = "") {
+
+            if (!File.Exists(templateFilename))
+                return ("No DOM static is property found on class.", null);
+
+            
+            string html = File.ReadAllText(templateFilename);
+
+            foreach (Match nestedScript in ScriptPattern.Matches(html))
+            {
+                var scriptMatch = nestedScript.Groups["script"];
+                if (scriptMatch.Success)
+                    js += "\r\n" + scriptMatch.Value;
+            }
+
+            if (string.IsNullOrEmpty(js))
+                return ("No JS content found.", null);
+
+            (string failReason, string domModule, string before, string after) = ParseJSContent(js);
+            if(!string.IsNullOrEmpty(failReason))
+                return (failReason, null);
+
+            var htmlImports = new List<string>();
+            foreach (Match htmlImport in HtmlImportPattern.Matches(html))
+            {
+                var href = htmlImport.Groups["href"];
+                if (href.Success)
+                    htmlImports.Add(href.Value);
+            }
+
+            var scripts = new List<string>();
+            int offset = 0;
+            foreach (Match nestedScript in NestedScriptPattern.Matches(html))
+            {
+                var src = nestedScript.Groups["src"];
+                if (src.Success)
+                    scripts.Add(src.Value);
+
+                string beforeScr = html.Substring(0, nestedScript.Index - offset);
+                string afterScr = html.Substring(nestedScript.Index + nestedScript.Length - offset);
+                html = beforeScr + afterScr;
+                offset += nestedScript.Length; // Strting now shorter by the removed length
+            }
+
+            string template = null;
+            foreach (Match domTemplate in DomModuleTemplatePattern.Matches(html))
+            {
+                var id = domTemplate.Groups["id"];
+                if (!id.Success)
                     continue;
-                }
 
+                if (domModule != id.Value)
+                    continue; // Not this module
 
-                string domModule = matchDomIsGroup.Value;
-                Console.WriteLine($"\tDOM module: {domModule}");
+                var t = domTemplate.Groups["template"];
+                if (!t.Success)
+                    break; // We know the ID matches, but template not found
 
-                var htmlImports = new List<string>();
-                string html = File.ReadAllText(templateFilename);
-                foreach (Match htmlImport in HtmlImportPattern.Matches(html))
+                template = t.Value;
+            }
+
+            if (string.IsNullOrEmpty(template))
+                return ($"No <template> with id matching {domModule} found in HTML.", null);
+
+            // Find <style includes="old-styles">...
+            // turn into const styles = `{oldStyles}...
+            string styles = "";
+            string[] includeStyles = new string[0];
+            var styleMatch = StylePattern.Match(template);
+            if (styleMatch.Success)
+            {
+                var includeMatch = styleMatch.Groups["include"];
+                if (includeMatch.Success)
                 {
-                    var href = htmlImport.Groups["href"];
-                    if (href.Success)
-                        htmlImports.Add(href.Value);
+                    includeStyles = includeMatch.Value.Split(' ');
+                    styles += string.Join("", includeStyles.Select(i => "${" + DashToCamelCase(i) + "}")) + "\r\n";
                 }
 
-                if (htmlImports.Count > 0)
-                    Console.WriteLine($"\tHTML Imports: x{htmlImports.Count}");
-
-                var scripts = new List<string>();
-                int offset = 0;
-                foreach (Match nestedScript in NestedScriptPattern.Matches(html))
-                {
-                    var src = nestedScript.Groups["src"];
-                    if (src.Success &&
-                        !jsRelativeFile.Equals(src.Value, StringComparison.OrdinalIgnoreCase)) // Not the JS we found the HTML from
-                        scripts.Add(src.Value);
-
-                    string before = html.Substring(0, nestedScript.Index - offset);
-                    string after = html.Substring(nestedScript.Index + nestedScript.Length - offset);
-                    html = before + after;
-                    offset += nestedScript.Length; // Strting now shorter by the removed length
-                }
-
-                if (scripts.Count > 0)
-                    Console.WriteLine($"\tScripts: {string.Join(", ", scripts)}");
-
-                string template = null;
-                foreach (Match domTemplate in DomModuleTemplatePattern.Matches(html))
-                {
-                    var id = domTemplate.Groups["id"];
-                    if (!id.Success)
-                        continue;
-
-                    if (domModule != id.Value)
-                        continue; // Not this module
-
-                    var t = domTemplate.Groups["template"];
-                    if (!t.Success)
-                        break; // We know the ID matches, but template not found
-
-                    template = t.Value;
-                }
-
-                if (string.IsNullOrEmpty(template))
-                {
-                    WriteWarn($"\tNo <template> with id matching {domModule} found in HTML.");
-                    continue;
-                }
-
-                // Get the relative location of the library to the HTML imports, and figure out the path to the new ES Modules from that
-                string relativeLibDir = $"../../{library}/"; // best guess
-                if (htmlImports.Count > 0)
-                {
-                    string libInPath = $"/{library}/";
-                    foreach (var i in htmlImports)
-                    {
-                        var idx = i.IndexOf(libInPath);
-                        if (idx < 0)
-                            continue;
-
-                        relativeLibDir = i.Substring(0, idx + libInPath.Length);
-                        break;
-                    }
-                }
-
-                // Find <style includes="old-styles">...
-                // turn into const styles = `{oldStyles}...
-                string styles = "";
-                string[] includeStyles = new string[0];
-                var styleMatch = StylePattern.Match(template);
-                if (styleMatch.Success) {
-                    var includeMatch = styleMatch.Groups["include"];
-                    if (includeMatch.Success) {
-                        includeStyles = includeMatch.Value.Split(' ');
-                        styles += string.Join("", includeStyles.Select(i => "${" + DashToCamelCase(i) + "}")) + "\r\n";
-                    }
-
-                    var cssMatch = styleMatch.Groups["css"];
-                    if (cssMatch.Success)
-                        styles += $@"<style>
+                var cssMatch = styleMatch.Groups["css"];
+                if (cssMatch.Success)
+                    styles += $@"<style>
     {cssMatch.Value}
 </style>";
 
-                    template = 
-                        template.Substring(0, styleMatch.Index) + 
-                        template.Substring(styleMatch.Index + styleMatch.Length);
+                template =
+                    template.Substring(0, styleMatch.Index) +
+                    template.Substring(styleMatch.Index + styleMatch.Length);
+            }
+
+            // Get the relative location of the library to the HTML imports, and figure out the path to the new ES Modules from that
+            string relativeLibDir = $"../../{library}/"; // best guess
+            if (htmlImports.Count > 0)
+            {
+                string libInPath = $"/{library}/";
+                foreach (var i in htmlImports)
+                {
+                    var idx = i.IndexOf(libInPath);
+                    if (idx < 0)
+                        continue;
+
+                    relativeLibDir = i.Substring(0, idx + libInPath.Length);
+                    break;
                 }
+            }
 
-                // Get string before and after where we'll insert the template
-                string beforeJS = js.Substring(0, matchDomIs.Index + matchDomIs.Length);
-                string afterJS = js.Substring(matchDomIs.Index + matchDomIs.Length);
-
-                // In the before remove and //@ts-check, as we'll add at start
-                beforeJS = TSCheckPattern.Replace(beforeJS, "");
-
-                // Replace the classname.
-                beforeJS = beforeJS.Replace("Polymer.Element", "PolymerElement");
-
-                // Put it all together - TS, Polymer 3 element, repurposed HTML imports, existing scripts, and add the template after the 
-                string jsCombined = $@"// @ts-check
+            // Put it all together - TS, Polymer 3 element, repurposed HTML imports, existing scripts, and add the template after the 
+            string result = $@"// @ts-check
 import {{ PolymerElement, html }} from '{relativeLibDir}@polymer/polymer/polymer-element.js';
 {string.Join('\n', htmlImports.Where(NotDeprecated).Select(i => HtmlResourceToESModule(i, includeStyles)))}
 {string.Join('\n', scripts.Select(JSResourceToESModule))}
 
 const styles = html`{styles}`;
 
-{beforeJS}
+{before}
     static get template() {{ return html`${{styles}}{template}`; }}
-{afterJS}";
-                // WriteColour(jsCombined, ConsoleColor.Gray, ConsoleColor.DarkGray);
+{after}";
 
-                File.WriteAllText(f, jsCombined, Encoding.UTF8);
-                File.Delete(templateFilename);
-                WriteSuccess($"\tTemplate ({jsCombined.Length} chars) Moved from:\r\n\t\t{templateFilename} to\r\n\t\t{f}");
-            }
-
-            Console.ReadLine();
+            return (null, result);
         }
 
         static string DashToCamelCase(string dash) =>
